eed6ce7ca9ece1f20804927e59aba883926c47f5
sachin <sachinrajputlive@gmail.com>
2022-07-24 13:59:00 +0530

    change link url to details page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -107,7 +107,7 @@ function HomePage() {
             <div className="row">
                 {photos.map((image, index) => (
                     <div key={index} className="col-md-4">
-                        <Link to={`/DetailsPage/${image.id}`} state={{ data: image, allPhoto: photos }} > <Card
+                        <Link to={`/infiniteScrolling/DetailsPage/${image.id}`} state={{ data: image, allPhoto: photos }} > <Card
                             onClick={openDetailPage}
                             className="antd-card"
                             hoverable
